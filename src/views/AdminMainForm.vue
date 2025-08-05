551e0bb1d5f84799c45d6e8eb94e6e98c2ef0f32
DESKTOP-8FEL777\user <chlhyn1990@gmail.com>
2025-08-05 14:32:32 +0900

    공통수정2

--- src/views/AdminMainForm.vue
+++ src/views/AdminMainForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    
+    관리자메인
   </div>
 </template>
 
 <script>
 export default {
-
+  
 };
 </script>
 
--- src/views/UserMainForm.vue
+++ src/views/UserMainForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    
+    사용자메인
   </div>
 </template>
 
